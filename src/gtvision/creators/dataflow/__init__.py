e0d475bd6f3cbad51e1b2d5dec42c5f79b1da139
Thibaut Durand <durand.tibo@gmail.com>
2023-06-28 20:57:47 -0700

    Add `IterableDataFlowCreator` (#30)

---
+++ src/gtvision/creators/dataflow/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+__all__ = ["BaseDataFlowCreator", "IterableDataFlowCreator"]
+
+from gtvision.creators.dataflow.base import BaseDataFlowCreator
+from gtvision.creators.dataflow.iterable import IterableDataFlowCreator
---
+++ src/gtvision/creators/dataflow/iterable.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+__all__ = ["IterableDataFlowCreator"]
+
+from collections.abc import Iterable
+from typing import TypeVar
+
+from gravitorch.engines.base import BaseEngine
+from gravitorch.experimental.dataflow import IterableDataFlow
+
+from gtvision.creators.dataflow.base import BaseDataFlowCreator
+
+T = TypeVar("T")
+
+
+class IterableDataFlowCreator(BaseDataFlowCreator):
+    def __init__(self, iterable: Iterable[T], **kwargs) -> None:
+        self._iterable = iterable
+        self._kwargs = kwargs
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__qualname__}()"
+
+    def create(self, engine: BaseEngine | None = None) -> IterableDataFlow[T]:
+        return IterableDataFlow(self._iterable, **self._kwargs)
