3ace88862c5868e08151e307a7a4c05bc28c9f36
Thibaut Durand <durand.tibo@gmail.com>
2023-06-28 21:50:38 -0700

    Add `DataLoaderDataFlowCreator` (#31)

--- src/gtvision/creators/dataflow/iterable.py
+++ src/gtvision/creators/dataflow/iterable.py
@@ -14,6 +14,14 @@ T = TypeVar("T")
 
 
 class IterableDataFlowCreator(BaseDataFlowCreator):
+    r"""Implements a simple ``IterableDataFlow`` creator.
+
+    Args:
+    ----
+        iterable (``Iterable``): Specifies the iterable.
+        **kwargs: See ``IterableDataFlow`` documentation.
+    """
+
     def __init__(self, iterable: Iterable[T], **kwargs) -> None:
         self._iterable = iterable
         self._kwargs = kwargs
