2d697aef16c062e5e8924270b38165f6b736c604
Thibaut Durand <durand.tibo@gmail.com>
2023-06-30 15:38:28 -0700

    Add `setup_dataflow_creator`

--- src/gtvision/creators/dataflow/base.py
+++ src/gtvision/creators/dataflow/base.py
@@ -1,19 +1,24 @@
 from __future__ import annotations
 
-__all__ = ["BaseDataFlowCreator"]
+__all__ = ["BaseDataFlowCreator", "setup_dataflow_creator"]
 
+import logging
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Generic, TypeVar
 
 from gravitorch.experimental.dataflow.base import BaseDataFlow
+from gravitorch.utils.format import str_target_object
+from objectory import AbstractFactory
 
 if TYPE_CHECKING:
     from gravitorch.engines import BaseEngine
 
+logger = logging.getLogger(__name__)
+
 T = TypeVar("T")
 
 
-class BaseDataFlowCreator(Generic[T], ABC):
+class BaseDataFlowCreator(Generic[T], ABC, metaclass=AbstractFactory):
     r"""Define the base class to implement a dataflow creator.
 
     Example usage:
@@ -38,3 +43,27 @@ class BaseDataFlowCreator(Generic[T], ABC):
         -------
             ``BaseDataFlow``: The created dataflow.
         """
+
+
+def setup_dataflow_creator(creator: BaseDataFlowCreator | dict) -> BaseDataFlowCreator:
+    r"""Sets up the dataflow creator.
+
+    The dataflow creator is instantiated from its configuration by
+    using the ``BaseDataFlowCreator`` factory function.
+
+    Args:
+    ----
+        creator (``BaseDataFlowCreator`` or dict): Specifies the
+            dataflow creator or its configuration.
+
+    Returns:
+    -------
+        ``BaseDataFlowCreator``: The instantiated dataflow creator.
+    """
+    if isinstance(creator, dict):
+        logger.info(
+            "Initializing the dataflow creator from its configuration... "
+            f"{str_target_object(creator)}"
+        )
+        creator = BaseDataFlowCreator.factory(**creator)
+    return creator
