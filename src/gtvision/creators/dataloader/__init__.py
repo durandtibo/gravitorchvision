43c9e492817043dc58d27eb108b48e00cf830544
Thibaut Durand <durand.tibo@gmail.com>
2023-07-02 18:52:15 -0700

    Improve `VanillaDataLoaderCreator`

--- src/gtvision/creators/dataloader/__init__.py
+++ src/gtvision/creators/dataloader/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
     "BaseDataLoaderCreator",
     "DataLoaderCreator",
     "DistributedDataLoaderCreator",
+    "VanillaDataLoaderCreator",
     "setup_data_loader_creator",
 ]
 
@@ -12,4 +13,7 @@ from gtvision.creators.dataloader.auto import AutoDataLoaderCreator
 from gtvision.creators.dataloader.base import BaseDataLoaderCreator
 from gtvision.creators.dataloader.distributed import DistributedDataLoaderCreator
 from gtvision.creators.dataloader.factory import setup_data_loader_creator
-from gtvision.creators.dataloader.vanilla import DataLoaderCreator
+from gtvision.creators.dataloader.vanilla import (
+    DataLoaderCreator,
+    VanillaDataLoaderCreator,
+)
--- src/gtvision/creators/dataloader/vanilla.py
+++ src/gtvision/creators/dataloader/vanilla.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
-__all__ = ["DataLoaderCreator"]
+__all__ = ["DataLoaderCreator", "VanillaDataLoaderCreator"]
 
 from typing import TypeVar
 
 from gravitorch.data.dataloaders import create_dataloader
 from gravitorch.data.datasets import is_dataset_config
 from gravitorch.engines import BaseEngine
+from gravitorch.utils import setup_object
 from gravitorch.utils.format import str_indent, str_pretty_dict
 from gravitorch.utils.seed import get_torch_generator
 from torch.utils.data import DataLoader, Dataset
@@ -56,3 +57,48 @@ class DataLoaderCreator(BaseDataLoaderCreator[T]):
             generator=get_torch_generator(self._seed + epoch),
             **self._kwargs,
         )
+
+
+class VanillaDataLoaderCreator(BaseDataLoaderCreator[T]):
+    r"""Implements a simple dataloader creator.
+
+    Args:
+    ----
+        dataloader (``torch.utils.data.DataLoader`` or dict): Specifies
+            the dataloader or its configuration.
+        cache (bool, optional): If ``True``, the dataloader is created
+            only the first time, and then the same data is returned
+            for each call to the ``create`` method.
+            Default: ``False``
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from gtvision.creators.dataloader import VanillaDataLoaderCreator
+        >>> from gravitorch.data.datasets import ExampleDataset
+        >>> dataset = ExampleDataset((1, 2, 3, 4))
+        >>> creator = VanillaDataLoaderCreator(
+        ...     {"_target_": "torch.utils.data.DataLoader", "dataset": dataset},
+        ... )
+        >>> creator.create()
+    )
+    """
+
+    def __init__(self, dataloader: DataLoader | dict, cache: bool = True) -> None:
+        self._dataloader = dataloader
+        self._cache = bool(cache)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__}(\n"
+            f"  dataloader={str_indent(self._dataloader)}\n"
+            f"  cache={self._cache},"
+            ")"
+        )
+
+    def create(self, engine: BaseEngine | None = None) -> DataLoader[T]:
+        dataloader = setup_object(self._dataloader)  # TODO: setup_dataloader
+        if self._cache and not isinstance(self._dataloader, DataLoader):
+            self._dataloader = dataloader
+        return dataloader
