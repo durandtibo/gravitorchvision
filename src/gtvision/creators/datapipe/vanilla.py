64a6580704dc3f3dbe91ac7bfeeccb709ddf7fa4
Thibaut Durand <durand.tibo@gmail.com>
2023-07-02 22:41:09 -0700

    Add `DataPipeCreator` (#50)

---
+++ src/gtvision/creators/datapipe/vanilla.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+__all__ = ["DataPipeCreator"]
+
+from collections.abc import Sequence
+from typing import TypeVar
+
+from gravitorch.datapipes import clone_datapipe, setup_datapipe
+from gravitorch.engines.base import BaseEngine
+from gravitorch.utils.format import str_indent
+from torch.utils.data import IterDataPipe, MapDataPipe
+
+from gtvision.creators.datapipe.base import BaseDataPipeCreator
+
+T = TypeVar("T")
+
+
+class DataPipeCreator(BaseDataPipeCreator[T]):
+    r"""Implements a simple ``DataPipe`` creator.
+
+    Args:
+    ----
+        datapipe (``IterDataPipe`` or ``MapDataPipe`` or dict):
+            Specifies the ``DataPipe`` or its configuration.
+        cache (bool, optional): If ``True``, the ``DataPipe`` is
+            created only the first time, and then the same
+            ``DataPipe`` is returned for each call to the
+            ``create`` method. Default: ``False``
+        deepcopy (bool, optional): If ``True``, the ``DataPipe``
+            object is deep-copied before to iterate over the data.
+            It allows a deterministic behavior when in-place
+            operations are performed on the data.
+            Default: ``False``
+    """
+
+    def __init__(
+        self,
+        datapipe: IterDataPipe[T] | MapDataPipe[T] | dict,
+        cache: bool = True,
+        deepcopy: bool = False,
+    ) -> None:
+        self._datapipe = datapipe
+        self._cache = bool(cache)
+        self._deepcopy = bool(deepcopy)
+
+    def __str__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__}(\n"
+            f"  datapipe={str_indent(self._datapipe)}\n"
+            f"  cache={self._cache},"
+            f"  deepcopy={self._deepcopy},"
+            ")"
+        )
+
+    def create(
+        self, engine: BaseEngine | None = None, source_inputs: Sequence | None = None
+    ) -> IterDataPipe[T] | MapDataPipe[T]:
+        datapipe = setup_datapipe(self._datapipe)
+        if self._cache and not isinstance(self._datapipe, (IterDataPipe, MapDataPipe)):
+            self._datapipe = datapipe
+        if self._deepcopy:
+            datapipe = clone_datapipe(datapipe, raise_error=False)
+        return datapipe
---
+++ tests/unit/creators/datapipe/test_vanilla.py
@@ -0,0 +1,64 @@
+from objectory import OBJECT_TARGET
+from torch.utils.data.datapipes.iter import IterableWrapper
+
+from gtvision.creators.datapipe import DataPipeCreator
+
+#####################################
+#     Tests for DataPipeCreator     #
+#####################################
+
+
+def test_datapipe_creator_str() -> None:
+    assert str(DataPipeCreator(IterableWrapper([1, 2, 3, 4, 5]))).startswith("DataPipeCreator(")
+
+
+def test_datapipe_creator_create_datapipe_object() -> None:
+    datapipe = IterableWrapper([1, 2, 3, 4, 5])
+    dp = DataPipeCreator(datapipe).create()
+    assert dp is datapipe
+    assert tuple(dp) == tuple(datapipe)
+
+
+def test_datapipe_creator_create_datapipe_config() -> None:
+    assert tuple(
+        DataPipeCreator(
+            {
+                OBJECT_TARGET: "torch.utils.data.datapipes.iter.IterableWrapper",
+                "iterable": [1, 2, 3, 4, 5],
+            }
+        ).create()
+    ) == tuple([1, 2, 3, 4, 5])
+
+
+def test_datapipe_creator_create_deepcopy() -> None:
+    datapipe = IterableWrapper([1, 2, 3, 4, 5])
+    dp = DataPipeCreator(datapipe, deepcopy=True).create()
+    assert dp is not datapipe
+    assert tuple(dp) == tuple(datapipe)
+
+
+def test_datapipe_creator_create_cache() -> None:
+    creator = DataPipeCreator(
+        {
+            OBJECT_TARGET: "torch.utils.data.datapipes.iter.IterableWrapper",
+            "iterable": [1, 2, 3, 4, 5],
+        }
+    )
+    datapipe1 = creator.create()
+    datapipe2 = creator.create()
+    assert datapipe1 is datapipe2
+    assert tuple(datapipe1) == tuple(datapipe2)
+
+
+def test_datapipe_creator_create_no_cache() -> None:
+    creator = DataPipeCreator(
+        {
+            OBJECT_TARGET: "torch.utils.data.datapipes.iter.IterableWrapper",
+            "iterable": [1, 2, 3, 4, 5],
+        },
+        cache=False,
+    )
+    datapipe1 = creator.create()
+    datapipe2 = creator.create()
+    assert datapipe1 is not datapipe2
+    assert tuple(datapipe1) == tuple(datapipe2)
