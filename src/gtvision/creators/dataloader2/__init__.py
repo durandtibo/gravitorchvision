00ee13450ee2628a61d7f21381b6de69a07d8909
Thibaut Durand <durand.tibo@gmail.com>
2023-07-04 22:45:37 -0700

    Add `setup_dataloader2_creator`

--- src/gtvision/creators/dataloader2/__init__.py
+++ src/gtvision/creators/dataloader2/__init__.py
@@ -1,8 +1,18 @@
 from __future__ import annotations
 
-__all__ = ["BaseDataLoader2Creator", "DataLoader2Creator", "VanillaDataLoader2Creator"]
+__all__ = [
+    "BaseDataLoader2Creator",
+    "DataLoader2Creator",
+    "VanillaDataLoader2Creator",
+    "is_dataloader2_creator_config",
+    "setup_dataloader2_creator",
+]
 
-from gtvision.creators.dataloader2.base import BaseDataLoader2Creator
+from gtvision.creators.dataloader2.base import (
+    BaseDataLoader2Creator,
+    is_dataloader2_creator_config,
+    setup_dataloader2_creator,
+)
 from gtvision.creators.dataloader2.vanilla import (
     DataLoader2Creator,
     VanillaDataLoader2Creator,
