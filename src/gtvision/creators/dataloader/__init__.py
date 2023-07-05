81207e10aaa0250fcef6ad6346d19f56802a0252
Thibaut Durand <durand.tibo@gmail.com>
2023-07-04 22:26:28 -0700

    Add `setup_dataloader_creator`

--- src/gtvision/creators/dataloader/__init__.py
+++ src/gtvision/creators/dataloader/__init__.py
@@ -14,9 +14,9 @@ from gtvision.creators.dataloader.auto import AutoDataLoaderCreator
 from gtvision.creators.dataloader.base import (
     BaseDataLoaderCreator,
     is_dataloader_creator_config,
+    setup_dataloader_creator,
 )
 from gtvision.creators.dataloader.distributed import DistributedDataLoaderCreator
-from gtvision.creators.dataloader.factory import setup_dataloader_creator
 from gtvision.creators.dataloader.vanilla import (
     DataLoaderCreator,
     VanillaDataLoaderCreator,
