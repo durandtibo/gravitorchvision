6f51f0ac2d2bacb521bffe2861f697beb2287017
Thibaut Durand <durand.tibo@gmail.com>
2023-07-04 23:14:34 -0700

    Add `is_dataflow_creator_config` (#65)

--- src/gtvision/creators/dataflow/__init__.py
+++ src/gtvision/creators/dataflow/__init__.py
@@ -4,9 +4,14 @@ __all__ = [
     "BaseDataFlowCreator",
     "IterableDataFlowCreator",
     "DataLoaderDataFlowCreator",
+    "is_dataflow_creator_config",
     "setup_dataflow_creator",
 ]
 
-from gtvision.creators.dataflow.base import BaseDataFlowCreator, setup_dataflow_creator
+from gtvision.creators.dataflow.base import (
+    BaseDataFlowCreator,
+    is_dataflow_creator_config,
+    setup_dataflow_creator,
+)
 from gtvision.creators.dataflow.dataloader import DataLoaderDataFlowCreator
 from gtvision.creators.dataflow.iterable import IterableDataFlowCreator
--- tests/unit/creators/dataflow/test_base.py
+++ tests/unit/creators/dataflow/test_base.py
@@ -2,7 +2,29 @@ from __future__ import annotations
 
 from objectory import OBJECT_TARGET
 
-from gtvision.creators.dataflow import IterableDataFlowCreator, setup_dataflow_creator
+from gtvision.creators.dataflow import (
+    IterableDataFlowCreator,
+    is_dataflow_creator_config,
+    setup_dataflow_creator,
+)
+
+################################################
+#     Tests for is_dataflow_creator_config     #
+################################################
+
+
+def test_is_dataflow_creator_config_true() -> None:
+    assert is_dataflow_creator_config(
+        {
+            OBJECT_TARGET: "gtvision.creators.dataflow.IterableDataFlowCreator",
+            "iterable": (1, 2, 3, 4, 5),
+        }
+    )
+
+
+def test_is_dataflow_creator_config_false() -> None:
+    assert not is_dataflow_creator_config({"_target_": "torch.nn.Identity"})
+
 
 ############################################
 #     Tests for setup_dataflow_creator     #
